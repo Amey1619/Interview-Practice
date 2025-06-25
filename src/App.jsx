b444f5d58b42169db7c49a3ec9f3102d054df55f
easyticketing <easyticketing30@gmail.com>
2025-06-25 20:00:09 +0530

    interview:Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
-import Header from "./components/Navbar/Header";
-import Home from "./components/Home/Home";
-import Posts from "./components/Postdetails/Posts"
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Card from "./components/Cards/Cards";
 import "./App.css";
-
 const App = () => {
-  return (
-    <Router>
-    <div className="app-container">
-      <Header />
-      <div className="content-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/post/:objectID" element={<Posts />} />
-          </Routes>
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const fetchData = async () => {
+    const jsonData = await fetch("https://dummyjson.com/products?limit=0").then(
+      (res) => res.json()
+    );
+    setProducts(jsonData.products);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const pageSize = 10;
+  const pageCount = Math.ceil(products.length / pageSize);
+  const start = currentPage * pageSize;
+  const end = start + pageSize;
+  return !products.length ? (
+    <div className="loading">Loading...</div>
+  ) : (
+    <div className="App">
+      <h1>Pagination</h1>
+      <div className="pages">
+        <button
+          className="arrow"
+          onClick={() =>
+            currentPage > 0 ? setCurrentPage((prev) => prev - 1) : ""
+          }
+        >
+          ◀️
+        </button>
+        {[...Array(pageCount).keys()].map((n) => (
+          <button
+            className={`page ${n === currentPage ? "active" : ""}`}
+            key={n}
+            onClick={() => setCurrentPage(n)}
+          >
+            {n + 1}
+          </button>
+        ))}
+        <button
+          className="arrow"
+          onClick={() =>
+            currentPage < pageSize ? setCurrentPage((prev) => prev + 1) : ""
+          }
+        >
+          ▶️
+        </button>
+      </div>
+      <div className="container">
+        {products.slice(start, end).map((p) => (
+          <Card key={p.id} image={p.thumbnail} title={p.title} />
+        ))}
       </div>
     </div>
-    </Router>
   );
 };
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,11 @@
 import "./custom.css";
-import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
-function Card({ objectID, title, author, date }) {
-  const updatedAtDate = new Date(date);
-  const formattedDate = updatedAtDate
-    .toLocaleDateString("en-US", {
-      weekday: "short",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    }).replace(/,/g, "");
+function Card({ image, title }) {
   return (
-    <Link to={`/post/${objectID}`} className="custom-card">
-      <div className="card-content">
-        <h3 style={{color:"black"}}>{!title ? "No Title Provided" : title}</h3>
-        <p style={{color:"black"}}>
-          Author:{" "}
-          <span style={{ fontFamily: "cursive"}}>
-            {!author ? "Unknown" : author}
-          </span>
-        </p>
-        <p style={{color:"purple"}}>{formattedDate}</p>
-      </div>
-    </Link>
+    <div className="custom-card">
+      <img src={image} alt={title} className="card-image" />
+      <span className="card-title">{title}</span>
+    </div>
   );
 }
 
